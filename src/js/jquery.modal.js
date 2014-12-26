6ffd78844ac32884a4ff0d13355c1a3432f09e99
Satyajit Sahoo <satyajit.happy@gmail.com>
2014-12-26 16:53:27 +0530

    Rework the modal plugin

--- src/js/jquery.modal.js
+++ src/js/jquery.modal.js
@@ -13,93 +13,130 @@ registerPlugin("modal", {
 	init: function() {
 		var self = this,
 			settings = self.settings,
+			$parent = $(settings.parent),
+			$oldmodal = $parent.find(".modal"),
+			$oldbackdrop = $parent.find(".backdrop"),
 			$modal = $(self.element).addClass("modal"),
-			$backdrop = (settings.backdrop) ? $("<div>").addClass("backdrop") : null;
+			$backdrop;
 
-		// There can be only one modal, so dismiss others
-		self.dismiss(true, function() {
-			// Add event listener to buttons which can dismiss the modal
-			$modal.find(".modal-remove").on("click", self.dismiss);
+		// Add event listener to buttons which can dismiss the modal
+		$modal.on("click.modal", ".modal-remove", function() {
+			self.dismiss();
+		});
 
-			// Add the modal to the DOM
-			// We are attaching it early so we can get width and height
-			// Which is needed for calculating position for centering
-			$modal.appendTo(settings.parent);
+		// Add the modal to the DOM
+		// We are attaching it early so we can get width and height
+		// Which is needed for calculating position for centering
+		if ($oldmodal.length) {
+			$oldmodal.replaceWith($modal);
+		} else {
+			$modal.appendTo($parent);
+		}
 
-			// Center the modal in the window
-			$modal.css({
-				"margin-top": $modal.outerHeight() / -2,
-				"margin-left": $modal.outerWidth() / -2
-			});
+		// Center the modal in the window
+		$modal.css({
+			"margin-top": $modal.outerHeight() / -2,
+			"margin-left": $modal.outerWidth() / -2
+		});
 
-			// Add a dark semi-transparent backdrop if specified
-			if ($backdrop) {
-				if (settings.dismiss) {
-					$backdrop.on("click", self.dismiss);
-				}
-
+		// Add a dark semi-transparent backdrop if specified
+		if (settings.backdrop) {
+			if ($oldbackdrop.length) {
+				$backdrop = $oldbackdrop;
+			} else {
+				$backdrop = $("<div>").addClass("backdrop");
 				$backdrop.appendTo(settings.parent);
 			}
 
-			// Add event listener to dismiss the modal
-			$(document).on("keydown.modal", function(e) {
-				if (e.which === 27 && settings.dismiss) {
+			if (settings.dismiss) {
+				$backdrop.on("click.modal", function() {
 					self.dismiss();
-				}
-			});
+				});
+			} else {
+				$backdrop.off("click.modal");
+			}
 
-			// Modal is now initialized
-			$.event.trigger("modalInited", [ $(self.element) ]);
+			$modal.data("modal-backdrop", $backdrop);
+		} else if ($oldbackdrop.length) {
+			$oldbackdrop.remove();
+		}
+
+		// Add event listener to dismiss the modal
+		$(document).off("keydown.modal").on("keydown.modal", function(e) {
+			if (e.which === 27 && settings.dismiss) {
+				self.dismiss();
+			}
 		});
+
+		// Modal is now initialized
+		$.event.trigger("modalInited", [ $(self.element) ]);
+	},
+
+	/**
+	 * Cleanup modal dialog.
+	 * @constructor
+	 */
+	destroy: function() {
+		var $modal = this.element ? $(this.element) : $(".modal");
+
+		// The element doesn't exist
+		if (!$modal.length) {
+			return;
+		}
+
+		// Cleanup event listeners
+		$modal.off("click.modal").css({
+			transform: "",
+			opacity: ""
+		}).removeClass("modal");
+
+		// Remove event listeners
+		$(document).off("keydown.modal");
 	},
 
 	/**
 	 * Dismiss modal dialog.
 	 * @constructor
 	 */
-	dismiss: function(replacing, callback) {
-		var $modal = $(".modal"),
-			$backdrop = $(".backdrop"),
-			triggerEvents = function(callback) {
-				var type = (replacing === true) ? "previousModalDismissed" : "modalDismissed";
+	dismiss: function() {
+		var self = this,
+			$modal = this.element ? $(this.element) : $(".modal"),
+			$backdrop = $modal.data("modal-backdrop"),
+			cleanup = function() {
+				// Remove modal and backdrop
+				if ($backdrop.length) {
+					$backdrop.remove();
+				}
+
+				$modal.remove();
+
+				self.destroy();
 
 				// Modal is now dismissed
-				$.event.trigger(type, [ $modal ]);
-
-				// Remove event listeners
-				$(document).off("keydown.modal");
-
-				return callback();
+				$.event.trigger("modalDismissed", [ $modal ]);
 			};
 
-		callback = (typeof callback === "function") ? callback : function() {};
+		$backdrop = ($backdrop && $backdrop.length) ? $backdrop : $(".backdrop");
 
 		// Element doesn't exist
-		if (!($modal.length || $backdrop.length)) {
-			return callback();
+		if (!($modal.length && $backdrop.length)) {
+			return;
 		}
 
 		// Remove the element from DOM
 		if ($.fn.velocity) {
 			if ($backdrop.length) {
-				$backdrop.velocity("fadeOut", 150, function() {
-					$backdrop.remove();
-				});
+				$backdrop.velocity("fadeOut", 150);
 			}
 
 			$modal.velocity({
 				opacity: 0,
-				scale: (replacing === true) ? "120%" : "70%"
+				scale: "70%"
 			}, 150, function() {
-				$modal.remove();
-
-				triggerEvents(callback);
+				cleanup();
 			});
 		} else {
-			$backdrop.remove();
-			$modal.remove();
-
-			triggerEvents(callback);
+			cleanup();
 		}
 	}
 });
